8158e587d7cfa2eef8b5bf5fcc136755cf640b91
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-09-22 17:42:24 +0600

    the change code

--- src/app/(auth)/verify-otp/page.tsx
+++ src/app/(auth)/verify-otp/page.tsx
@@ -1,11 +1,5 @@
-import VerifyEmailForm from "@/components/authentication/Verifyemailform/Verifyemailform";
-
 const page = () => {
-  return (
-    <div>
-      <VerifyEmailForm />
-    </div>
-  );
+  return <div>varfigation</div>;
 };
 
 export default page;
