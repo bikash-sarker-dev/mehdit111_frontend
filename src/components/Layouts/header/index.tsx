0d3e0d5ded9b4544038e8c3b89d5eea627647923
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-09-23 14:18:40 +0600

    dashboard header working

--- src/components/Layouts/header/index.tsx
+++ src/components/Layouts/header/index.tsx
@@ -11,6 +11,11 @@ import { UserInfo } from "./user-info";
 
 export function Header() {
   const { toggleSidebar, isMobile } = useSidebarContext();
+  const USER = {
+    name: "John Smith",
+    email: "johnson@nextadmin.com",
+    img: "/images/user/user-03.png",
+  };
 
   return (
     <header className="sticky top-0 z-30 flex items-center justify-between border-b border-stroke bg-white px-4 py-5 shadow-1 dark:border-stroke-dark dark:bg-gray-dark md:px-5 2xl:px-10">
@@ -38,26 +43,34 @@ export function Header() {
         <h1 className="mb-0.5 text-heading-5 font-bold text-dark dark:text-white">
           Dashboard
         </h1>
-        <p className="font-medium">Next.js Admin Dashboard Solution</p>
+        {/* <p className="font-medium">Next.js Admin Dashboard Solution</p> */}
       </div>
 
       <div className="flex flex-1 items-center justify-end gap-2 min-[375px]:gap-4">
-        <div className="relative w-full max-w-[300px]">
-          <input
-            type="search"
-            placeholder="Search"
-            className="flex w-full items-center gap-3.5 rounded-full border bg-gray-2 py-3 pl-[53px] pr-5 outline-none transition-colors focus-visible:border-primary dark:border-dark-3 dark:bg-dark-2 dark:hover:border-dark-4 dark:hover:bg-dark-3 dark:hover:text-dark-6 dark:focus-visible:border-primary"
-          />
-
-          <SearchIcon className="pointer-events-none absolute left-5 top-1/2 -translate-y-1/2 max-[1015px]:size-5" />
-        </div>
-
-        <ThemeToggleSwitch />
-
         <Notification />
 
         <div className="shrink-0">
-          <UserInfo />
+          {/* <UserInfo /> */}
+          <figure className="flex items-center gap-2.5 px-5 py-3.5">
+            <Image
+              src={USER.img}
+              className="size-10"
+              alt={`Avatar for ${USER.name}`}
+              role="presentation"
+              width={200}
+              height={200}
+            />
+
+            <figcaption className="space-y-1 text-base font-medium">
+              <div className="mb-2 leading-none text-dark dark:text-white">
+                {USER.name}
+              </div>
+
+              <div className="text-xs leading-none text-gray-6">
+                {USER.email}
+              </div>
+            </figcaption>
+          </figure>
         </div>
       </div>
     </header>
